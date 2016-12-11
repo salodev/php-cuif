4d8dd6ec370694132274429c6e2a5704846f1c4f
salomon <salomon@salomon-MS-7641>
2016-12-11 13:04:44 -0300

    moved and separated into repos.

--- bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-set_time_limit(0);
-spl_autoload_register(function($className) {
-	$className = str_replace('\\','/', $className);
-    $classFile = dirname(__FILE__) . "/{$className}.class.php";
-    if (file_exists($classFile)) {
-        require_once($classFile);
-    }
-});
--- lib/IMysqlConnection.interface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-interface IMysqlConnection {
-	public function __construct($host, $user, $password, $dbname);
-	public function connect();
-	public function selectDB($dbName);
-	public function query($query, $callback);
-	public function killQuery();
-}
